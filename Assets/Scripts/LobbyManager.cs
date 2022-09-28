bb4870211387b249dd932a7a368103c851f64072
Dorian Wolff <dorian.wolff@gmail.com>
2022-09-29 01:27:15 +0200

    functionning lobby

--- Assets/Scripts/LobbyManager.cs
+++ Assets/Scripts/LobbyManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
 using TMPro;
+using UnityEngine.PlayerLoop;
 using UnityEngine.UI;
 
 public class LobbyManager : MonoBehaviourPunCallbacks
@@ -14,6 +15,9 @@ public class LobbyManager : MonoBehaviourPunCallbacks
     public GameObject roomPanel;
     public TextMeshProUGUI roomName;
 
+    public RoomItem RoomItemPrefab;
+    private List<RoomItem> roomItemsList = new List<RoomItem>();
+    public Transform contentObject;
     private void Start()
     {
         PhotonNetwork.JoinLobby();
@@ -32,5 +36,26 @@ public class LobbyManager : MonoBehaviourPunCallbacks
         lobbyPanel.SetActive(false);
         roomPanel.SetActive(true);
         roomName.text = "Room Name : " + PhotonNetwork.CurrentRoom.Name;
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        UpdateRoomList(roomList);
+    }
+
+    void UpdateRoomList(List<RoomInfo> list)
+    {
+        foreach (RoomItem item in roomItemsList)
+        {
+            Destroy(item.gameObject);
+        }
+        roomItemsList.Clear();
+
+        foreach (RoomInfo room in list)
+        {
+            RoomItem newRoom = Instantiate(RoomItemPrefab, contentObject);
+            newRoom.SetRoomName(room.Name);
+            roomItemsList.Add(newRoom);
+        }
     }
 }
